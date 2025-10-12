bd28650a7d911313bea6853e81d7ddb05f78c31f
genuinebasilnt <genuinebnt@gmail.com>
2025-10-12 11:18:59 +0530

    fix tab completion

---
+++ lua/plugins/blink-cmp.lua
@@ -0,0 +1,25 @@
+return {
+  -- Configure blink.cmp with Tab to accept, arrows for navigation
+  {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        preset = "default",
+        -- Tab accepts the current selection
+        ["<Tab>"] = { "accept", "fallback" },
+        ["<S-Tab>"] = { "accept", "fallback" },
+        -- Enter also accepts
+        ["<CR>"] = { "accept", "fallback" },
+        -- Arrow keys for navigation
+        ["<Up>"] = { "select_prev", "fallback" },
+        ["<Down>"] = { "select_next", "fallback" },
+        -- Ctrl+p/n for navigation (Vim-style)
+        ["<C-p>"] = { "select_prev", "fallback" },
+        ["<C-n>"] = { "select_next", "fallback" },
+        -- Cancel completion
+        ["<C-e>"] = { "cancel", "fallback" },
+        ["<Esc>"] = { "cancel", "fallback" },
+      },
+    },
+  },
+}
